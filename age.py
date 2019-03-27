a6bf95446c114a35f34af434ba37aabdf1d36ef1
ks2019675069 <48661381+ks2019675069@users.noreply.github.com>
2019-03-27 16:44:13 +0900

    Add files via upload

---
+++ age.py
@@ -0,0 +1,5 @@
+name = input("이름: ")
+year = int(input("출생년도: "))
+age = 2019 - year + 1
+
+print(name,"님의 나이는", age, "세 입니다.")
---
+++ asmd.py
@@ -0,0 +1,7 @@
+num1 = int(input("첫 번째 정수: "))
+num2 = int(input("두 번째 정수: "))
+
+print(num1, "+", num2, "=",(num1+num2))
+print(num1, "-", num2, "=",(num1-num2))
+print(num1, "*", num2, "=",(num1*num2))
+print(num1, "/", num2, "=",(num1/num2))
